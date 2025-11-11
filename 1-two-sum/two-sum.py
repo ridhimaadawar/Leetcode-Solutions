d9971945b0efd9adbd5f28547f845cd69373de40
Ridhimaa Dawar <139648390+ridhimaadawar@users.noreply.github.com>
2025-11-11 20:40:36 +0530

    Time: 2052 ms (28.52%) | Memory: 13.2 MB (59.86%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -7,9 +7,5 @@ class Solution:
                     return [i, j]  
         return [] 
 
-if __name__ == "__main__":
-    nums = [2, 7, 11, 15]
-    target = 9
-    result = Solution().twoSum(nums, target)
-    print(result)
+
 
