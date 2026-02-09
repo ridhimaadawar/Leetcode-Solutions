645c097416fddcec5ca0b95b6a99512123a4f8ac
Ridhimaa Dawar <139648390+ridhimaadawar@users.noreply.github.com>
2026-02-09 19:26:54 +0530

    Time: 268 ms (18.97%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,6 @@
+SELECT
+    machine_id,
+    ROUND(AVG(CASE WHEN activity_type = 'end' THEN timestamp END)
+        - AVG(CASE WHEN activity_type = 'start' THEN timestamp END), 3) AS processing_time
+FROM Activity
+GROUP BY machine_id;
