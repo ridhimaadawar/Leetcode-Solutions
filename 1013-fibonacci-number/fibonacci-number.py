2216d3063354fd5ec51c9e9a54b03a07241a5d9a
Ridhimaa Dawar <139648390+ridhimaadawar@users.noreply.github.com>
2026-03-11 01:17:38 +0530

    Time: 22 ms (44.46%) | Memory: 12.4 MB (48.75%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    @staticmethod
+    def fib(n):
+        if n <= 1:
+            return n
+        
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        
+        return b
