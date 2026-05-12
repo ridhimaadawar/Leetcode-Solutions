0504052aa7abc185ae1f9bd1ccbce13f073a9dcc
Ridhimaa Dawar <139648390+ridhimaadawar@users.noreply.github.com>
2026-05-12 16:08:30 +0530

    Time: 14 ms (46.56%) | Memory: 19.4 MB (43.75%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        # If lengths differ, cannot be anagrams
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        # Count characters in s
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+
+        # Subtract counts using t
+        for ch in t:
+            if ch not in count:
+                return False
+
+            count[ch] -= 1
+
+            if count[ch] < 0:
+                return False
+
+        return True
