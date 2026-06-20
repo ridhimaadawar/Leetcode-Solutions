5027f62020eaf96f4d6c1c385408b5dbe13247ca
Ridhimaa Dawar <139648390+ridhimaadawar@users.noreply.github.com>
2026-06-20 14:09:01 +0530

    Time: 281 ms (27.27%) | Memory: 46.4 MB (54.55%) - LeetSync

---
+++ 1968-maximum-building-height/maximum-building-height.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxBuilding(self, n: int, restrictions: List[List[int]]) -> int:
+        restrictions.append([1, 0])
+
+        # Building n can never exceed n-1
+        restrictions.append([n, n - 1])
+
+        restrictions.sort()
+
+        m = len(restrictions)
+
+        # Left -> Right
+        for i in range(1, m):
+            dist = restrictions[i][0] - restrictions[i - 1][0]
+            restrictions[i][1] = min(
+                restrictions[i][1],
+                restrictions[i - 1][1] + dist
+            )
+
+        # Right -> Left
+        for i in range(m - 2, -1, -1):
+            dist = restrictions[i + 1][0] - restrictions[i][0]
+            restrictions[i][1] = min(
+                restrictions[i][1],
+                restrictions[i + 1][1] + dist
+            )
+
+        ans = 0
+
+        for i in range(1, m):
+            x1, h1 = restrictions[i - 1]
+            x2, h2 = restrictions[i]
+
+            dist = x2 - x1
+
+            # Highest peak possible between the two endpoints
+            ans = max(ans, (h1 + h2 + dist) // 2)
+
+        return ans
